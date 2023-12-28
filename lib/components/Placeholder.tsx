8f70cdc603ae96c8026dc73dfaabd75de6a117b3
tyro-coder <tyrocoder@163.com>
2023-12-28 18:41:56 +0800

    fix: Placeholder

--- lib/components/Placeholder.tsx
+++ lib/components/Placeholder.tsx
@@ -10,6 +10,8 @@ const Wrapper = styled.div`
   z-index: 0;
   display: none;
   transition: left 0.3s, top 0.3s, width 0.3s, height 0.3s;
+  border-style: solid;
+  border-color: transparent;
   &.active {
     display: block;
   }
@@ -39,13 +41,18 @@ const Placeholder: React.FC<IPlaceholderProps> = (props) => {
   });
 
   React.useEffect(() => {
-    if (!widgetRect || !widgetRef.current) return;
+    if (!widgetRect || !widgetRef.current || !layoutData) return;
     const { left, top, width, height } = widgetRect;
+    const { gap } = layoutData;
     widgetRef.current.style.width = `${width}px`;
     widgetRef.current.style.height = `${height}px`;
     widgetRef.current.style.top = `${top}px`;
     widgetRef.current.style.left = `${left}px`;
-  }, [widgetRect]);
+    widgetRef.current.style.borderLeftWidth = gap ? `${gap[0] * 0.5}px` : '0';
+    widgetRef.current.style.borderRightWidth = gap ? `${gap[0] * 0.5}px` : '0';
+    widgetRef.current.style.borderTopWidth = gap ? `${gap[1] * 0.5}px` : '0';
+    widgetRef.current.style.borderBottomWidth = gap ? `${gap[1] * 0.5}px` : '0';
+  }, [layoutData, widgetRect]);
 
   return (
     <Wrapper ref={widgetRef} className={cls({ active: show })}>
